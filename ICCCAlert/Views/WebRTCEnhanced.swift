4358d56b6095ab00b9665474ba63fd7d252b3664
saurav0701 <sauravbhardwaj2000@gmail.com>
2026-01-03 12:14:26 +0530

    fix :Removed thumbnails-1

--- ICCCAlert/Views/WebRTCEnhanced.swift
+++ ICCCAlert/Views/WebRTCEnhanced.swift
@@ -305,7 +305,7 @@ class StreamSession: ObservableObject {
         webView = nil
         startTime = nil
         
-        for _ in 0.../3 {
+        for _ in 0..<3 {
             autoreleasepool {}
         }
         
